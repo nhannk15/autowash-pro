ddd10162d17e5d448008721e7b55d49d30c734f0
MeoSatThu12 <baothi762@gmail.com>
2026-05-28 20:49:22 +0700

    feature: setup router

--- frontend/src/components/Layout/Header/Navbar.jsx
+++ frontend/src/components/Layout/Header/Navbar.jsx
@@ -1,10 +1,10 @@
+import { NavLink } from 'react-router-dom'
 import './Navbar.css'
 
 const navLinks = [
-    { label: 'Trang Chủ', href: '#', active: true },
-    { label: 'Dịch Vụ', href: '#', active: false },
-    { label: 'Hệ Thống', href: '#', active: false },
-    { label: 'Blog', href: '#', active: false },
+    { label: 'Trang Chủ', to: '/' },
+    { label: 'Dịch Vụ',   to: '/dich-vu' },
+    { label: 'Blog',       to: '/blog' },
 ]
 
 export default function NavBar() {
@@ -18,11 +18,23 @@ export default function NavBar() {
                 </div>
 
                 <ul className="navbar__links">
-                    {navLinks.map((value) => (<li key={value.label}><a className={`navbar__link ${value.active ? 'navbar_link--active' : undefined}`} href='#'>{value.label}</a></li>))}
+                    {navLinks.map((item) => (
+                        <li key={item.to}>
+                            <NavLink
+                                to={item.to}
+                                end={item.to === '/'}
+                                className={({ isActive }) =>
+                                    `navbar__link${isActive ? ' navbar_link--active' : ''}`
+                                }
+                            >
+                                {item.label}
+                            </NavLink>
+                        </li>
+                    ))}
                 </ul>
 
                 <div className="navbar__auth">
-                    <a href="#" className="navbar__btn navbar__btn--login">Login</a>
+                    <NavLink to="/login" className="navbar__btn navbar__btn--login">Login</NavLink>
                     <a href="#" className="navbar__btn navbar__btn--signup">Sign Up</a>
                 </div>
             </div>
