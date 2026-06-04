0220ed178366f5c9f0e02840c0fda4aed4222968
William <nhannk15@gmail.com>
2026-06-04 07:22:18 +0700

    feat: create API for create booking (no exceception handling)

--- backend/src/main/java/com/autowashpro/backend/repository/ServicePriceRepository.java
+++ backend/src/main/java/com/autowashpro/backend/repository/ServicePriceRepository.java
@@ -1,10 +1,26 @@
 package com.autowashpro.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.autowashpro.backend.model.entity.ServicePrice;
 
 @Repository
 public interface ServicePriceRepository extends JpaRepository<ServicePrice, Long> {
+
+    @Query("""
+                SELECT sp FROM ServicePrice sp
+                WHERE sp.id IN :ids
+                AND sp.vehicleType.id = :vehicleTypeId
+                AND sp.isActive = true
+            """)
+    List<ServicePrice> findActiveByIdsAndVehicleTypeId(
+            @Param("ids") List<Long> ids,
+            @Param("vehicleTypeId") Long vehicleTypeId);
+
+    
 }
