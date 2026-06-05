242e103b1c65f2745afb8b8b34df0aa2aefe23dd
William <nhannk15@gmail.com>
2026-06-05 16:28:40 +0700

    feat: add service image URL, service highlights, service steps

---
+++ backend/src/main/java/com/autowashpro/backend/model/entity/Step.java
@@ -0,0 +1,39 @@
+package com.autowashpro.backend.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "steps")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Step {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "step", nullable = false)
+    private Long step;
+
+    @Column(name = "step_description", nullable = false)
+    private String stepDescription;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "service_id", nullable = false)
+    private Service service;
+
+}
