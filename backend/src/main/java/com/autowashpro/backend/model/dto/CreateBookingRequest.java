dcf31e9d321d6dac7f9bd8f5ca065757974b0f90
William <nhannk15@gmail.com>
2026-06-09 10:27:01 +0700

    fix: add user ID to the response for context storing

--- backend/src/main/java/com/autowashpro/backend/model/dto/CreateBookingRequest.java
+++ backend/src/main/java/com/autowashpro/backend/model/dto/CreateBookingRequest.java
@@ -1,28 +1,29 @@
 package com.autowashpro.backend.model.dto;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
-import jakarta.validation.constraints.NotNull;
+import com.autowashpro.backend.model.entity.TimeSlot;
+
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class CreateBookingRequest {
 
-    @NotNull
+    private Long customerId;
+
     private Long vehicleId;
 
-    @NotNull
-    private LocalDateTime scheduledDateTime;
+    private TimeSlot timeSlotId;
 
-    @NotNull
     @Size(min = 1)
     private List<Long> servicePriceIds;
 
--- backend/src/main/java/com/autowashpro/backend/service/BookingService.java
+++ backend/src/main/java/com/autowashpro/backend/service/BookingService.java
@@ -7,12 +7,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.autowashpro.backend.exception.UserNotFoundException;
+import com.autowashpro.backend.model.dto.CreateBookingRequest;
+import com.autowashpro.backend.model.dto.CreateBookingResponse;
 import com.autowashpro.backend.model.dto.SlotAvailabilityByDateResponse;
 import com.autowashpro.backend.model.dto.TimeSlotAvailabilityResponse;
+import com.autowashpro.backend.model.entity.Customer;
 import com.autowashpro.backend.repository.AvailableSlotRepository;
+import com.autowashpro.backend.repository.CustomerRepository;
 
 @Service
 public class BookingService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @Autowired
     private AvailableSlotRepository availableSlotRepository;
@@ -39,6 +47,15 @@ public class BookingService {
                         .isAvailable(((Number) row[4]).intValue() > 0)
                         .build())
                 .toList();
+    }
+
+    private CreateBookingResponse createBooking(CreateBookingRequest createBookingRequest) {
+        Customer customer = customerRepository.findById(createBookingRequest.getCustomerId())
+                .orElseThrow(() -> new UserNotFoundException("Customer not found!"));
+
+        int bookingWindowDays = customer.getTier().getBookingWindowDays();
+        LocalDate now = LocalDate.now();
+        return null;
     }
 
 }
