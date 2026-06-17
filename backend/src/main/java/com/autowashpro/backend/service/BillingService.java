41b8a247904b1e46ecd6ead0a2c32b4276f64475
William <nhannk15@gmail.com>
2026-06-17 22:21:42 +0700

    feat: add API for Billing Payment Completion Using CASH method

--- backend/src/main/java/com/autowashpro/backend/service/BillingService.java
+++ backend/src/main/java/com/autowashpro/backend/service/BillingService.java
@@ -1,5 +1,6 @@
 package com.autowashpro.backend.service;
 
+import com.autowashpro.backend.repository.WashSessionRepository;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -16,29 +17,34 @@ import com.autowashpro.backend.model.entity.Billing;
 import com.autowashpro.backend.model.entity.Booking;
 import com.autowashpro.backend.model.entity.BookingDetail;
 import com.autowashpro.backend.model.entity.Voucher;
+import com.autowashpro.backend.model.entity.WashSession;
 import com.autowashpro.backend.model.enums.PaymentMethod;
 import com.autowashpro.backend.model.enums.PaymentStatus;
+import com.autowashpro.backend.model.enums.WashSessionStatus;
 import com.autowashpro.backend.repository.BillingRepository;
 import com.autowashpro.backend.repository.BookingRepository;
 
 @Service
 public class BillingService {
 
+    private final WashSessionRepository washSessionRepository;
     private final BillingRepository billingRepository;
     private final BookingRepository bookingRepository;
     private final BillingMapper billingMapper;
 
     @Autowired
     public BillingService(BillingRepository billingRepository, BookingRepository bookingRepository,
-            BillingMapper billingMapper) {
+            BillingMapper billingMapper, WashSessionRepository washSessionRepository) {
         this.billingRepository = billingRepository;
         this.bookingRepository = bookingRepository;
         this.billingMapper = billingMapper;
+        this.washSessionRepository = washSessionRepository;
     }
 
     public Billing createPendingBilling(Long bookingId) {
         Booking booking = bookingRepository.findById(bookingId)
-                .orElseThrow(() -> new BookingNotFoundException("Không tìm thấy lịch hẹn với id: " + bookingId));
+                .orElseThrow(() -> new BookingNotFoundException(
+                        "Không tìm thấy lịch hẹn với id: " + bookingId));
 
         Voucher voucher = null;
 
@@ -83,10 +89,28 @@ public class BillingService {
         List<Billing> billings = new ArrayList<>();
         for (Long bookingId : bookingIds) {
             Billing billing = billingRepository.findByBookingId(bookingId).orElseThrow(
-                    () -> new BillingNotFoundException("Không tìm thấy hóa đơn với id lịch đặt là: " + bookingId));
+                    () -> new BillingNotFoundException(
+                            "Không tìm thấy hóa đơn với id lịch đặt là: " + bookingId));
             billings.add(billing);
         }
         return billingMapper.toBillingResponses(billings);
+    }
+
+    public BillingResponse completeBillingUsingCashMethod(Long billingId) {
+        Billing billing = billingRepository.findById(billingId)
+                .orElseThrow(() -> new BillingNotFoundException(
+                        "Hóa đơn " + billingId + " không tồn tại"));
+        
+        billing.setPaymentStatus(PaymentStatus.PAID);
+        billing.setPaidAt(LocalDateTime.now());
+
+        for (WashSession washSession: billing.getBooking().getWashSessions()) {
+            washSession.setStatus(WashSessionStatus.PAID);
+            washSessionRepository.save(washSession);
+        }
+
+        Billing savedBilling = billingRepository.save(billing);
+        return billingMapper.toBillingResponse(savedBilling);
     }
 
 }
